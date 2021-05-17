3aa1974e33859782090e9c90247ed67c199ce05d
Denmey <Denmey98@gmail.com>
2021-05-17 21:55:53 +0300

    Forwarding previous tasks

--- runtime/runtime.c
+++ runtime/runtime.c
@@ -75,6 +75,7 @@ int Llength (void *p) {
   return BOX(LEN(a->tag));
 }
 
+// bn - args number (valuesN+1tag); ... - values + tag
 extern void* Bsexp (int bn, ...) {
   va_list args; 
   int     i;    
@@ -150,7 +151,7 @@ void* Belem (void *p, int i0) {
   return (void*) ((int*) a->contents)[i];
 }
 
-void* Bsta (void *x, int i, void *v) {
+void* Bsta (void *v, int i, void *x) {
   if (UNBOXED(i)) {
     if (TAG(TO_DATA(x)->tag) == STRING_TAG)((char*) x)[UNBOX(i)] = (char) UNBOX(v);
     else ((int*) x)[UNBOX(i)] = (int) v;
