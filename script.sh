66925f0ca741d18c26beecf35e38509a4dee6d5b
tretikoff <treticoff@gmail.com>
2021-11-14 18:01:51 +0300

    sm x86

--- script.sh
+++ script.sh
@@ -1,1 +1,1 @@
-docker run --mount type=bind,source=/Users/Konstantin.Tretiakov/compilers-2021-autumn,target=/compilers-2021-autumn -it berezun/cw-2021
+docker run --mount type=bind,source=$(pwd),target=/cmp -it berezun/cw-2021
