06f97dbc27d8cfd58345a658d202443a5a3df5cf
Kirill <bitikit@gmail.com>
2021-12-11 14:58:08 +0300

    Implement assembly-part

--- runtime/gc_runtime.s
+++ runtime/gc_runtime.s
@@ -30,14 +30,31 @@ L__gc_init:		movl	%esp, __gc_stack_bottom
 // then  set @__gc_stack_top to %ebp
 // else  return
 __pre_gc:
-			call nimpl
+			pushl %eax
+			movl  __gc_stack_top, %eax
+			cmpl  $0, %eax
+			jne __pre_gc_end
+			movl %ebp, __gc_stack_top
+
+__pre_gc_end:
+			popl  %eax
+			ret
+
 
 // ==================================================
 // if __gc_stack_top was set by one of the callers
 // then return
 // else set __gc_stack_top to 0
 __post_gc:
-			call nimpl
+			pushl %eax
+			movl  __gc_stack_top, %eax
+			cmpl  %ebp, %eax
+			jne   __post_gc_end
+			movl  $0, __gc_stack_top
+
+__post_gc_end:
+			popl %eax
+			ret
 
 // ==================================================
 // Scan stack for roots
@@ -45,4 +62,59 @@ __post_gc:
 // till __gc_stack_bottom
 // and calls gc_test_and_copy_root for each found root
 __gc_root_scan_stack:
-			call nimpl
+			pushl %ebp
+			pushl %ebx
+			pushl %ecx
+			movl  %esp, %ebp
+			movl  __gc_stack_top, %eax
+			sub   $4, %eax
+			jmp   __gc_root_scan_stack_next_element
+
+__gc_root_scan_stack_next_element:
+			add   $4, %eax
+			cmpl  %eax, __gc_stack_bottom
+			jne   __gc_root_scan_stack_process
+			jmp   __gc_root_scan_stack_end
+
+__gc_root_scan_stack_process:
+			movl  (%eax), %ebx
+
+__gc_root_scan_stack_code_section_1:
+			cmpl  __executable_start, %ebx
+			jnb   __gc_root_scan_stack_code_section_2
+			jmp   __gc_root_scan_stack_program_stack_1
+
+__gc_root_scan_stack_code_section_2:
+			cmpl  __etext, %ebx
+			jna   __gc_root_scan_stack_next_element
+			jmp   __gc_root_scan_stack_program_stack_1
+
+__gc_root_scan_stack_program_stack_1:
+			cmpl  __gc_stack_top, %ebx
+			jnb   __gc_root_scan_stack_program_stack_2
+			jmp   __gc_root_scan_stack_pointer
+
+__gc_root_scan_stack_program_stack_2:
+			cmpl  __gc_stack_bottom, %ebx
+			jna   __gc_root_scan_stack_next_element
+			jmp   __gc_root_scan_stack_pointer
+
+__gc_root_scan_stack_pointer:
+			andl  $0x00000001, %ebx
+			jnz   __gc_root_scan_stack_next_element
+			jmp   __gc_root_scan_stack_copy
+
+__gc_root_scan_stack_copy:
+			pushl %eax
+			pushl %eax
+			call  gc_test_and_copy_root
+			addl  $4, %esp
+			popl  %eax
+
+__gc_root_scan_stack_end:
+			movl  $0, %eax
+			movl  %ebp, %esp
+			popl  %ecx
+			popl  %ebx
+			popl  %ebp
+			ret
