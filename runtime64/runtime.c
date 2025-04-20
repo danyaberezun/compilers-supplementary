ed53ececbfc60cbb4846a4b0db6d956359f7bfeb
Dmitry Boulytchev <dboulytchev@gmail.com>
2025-04-20 17:22:43 +0300

    Assignment

--- runtime64/runtime.c
+++ runtime64/runtime.c
@@ -1,12 +1,16 @@
 # include <stdio.h>
 
+# define ALIGN_STACK asm ("andq $0xFFFFFFFFFFFFFFF0,%rsp")
+
 void Lwrite (long x) {
+  ALIGN_STACK;
   printf ("%ld\n", x);
 }
 
 long Lread () {
   long result;
 
+  ALIGN_STACK;
   scanf  ("%ld", &result);
   
   return result;
