ab67a1ecdc2cde67f74b4a1c741cfbbbdba4d03b
Dmitry Boulytchev <dboulytchev@gmail.com>
2025-12-22 14:28:00 +0300

    Fixed (?) int-to-void* conversion in runtime

--- runtime64/runtime.c
+++ runtime64/runtime.c
@@ -52,7 +52,7 @@ void* Bsexp (long k, ...) {
   
   for (i = 0; i<=n; i++) {
     ai = va_arg(args, long);
-    if (i == n) r->tag = ai;
+    if (i == n) r->tag = (void*) ai;
     else ((long*) r->data.contents)[i] = ai;
   }
   
