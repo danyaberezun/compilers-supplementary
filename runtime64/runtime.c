654ffc7f9643d277f2b8b8c8f2fc471c38ae1876
Vasily Fedorov <vasily.fedorov@arsysop.ru>
2026-04-11 12:58:56 +0300

    sync runtime

--- runtime64/runtime.c
+++ runtime64/runtime.c
@@ -1,18 +1,13 @@
 # include <stdio.h>
 
-# define ALIGN_STACK asm ("andq $0xFFFFFFFFFFFFFFF0,%rsp")
-
 void Lwrite (long x) {
-  ALIGN_STACK;
   printf ("%ld\n", x);
 }
 
 long Lread () {
   long result;
 
-  ALIGN_STACK;
   scanf ("%ld", &result);
   
   return result;
 }
-
